ce9d27d1c9bf579b2b71c7d06b201b9456e80f12
GOWTHAM KUMAR V <89139024+Gowtham212088@users.noreply.github.com>
2022-07-14 11:12:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT||5000;
 
 const MONGO_URL = process.env.MONGO_URL;
 
@@ -22,7 +22,7 @@ app.use(express.json());
 
 // ? CORS Third party middleware
 
-app.use(cors());
+app.use(cors({ origin: "*", }));
 
 const client = await createConnection();
 
